bcd6ce0c0c509d37732ce166025770dcc316d313
Erik Davidson <erik@erikd.org>
2016-11-13 15:25:13 -0600

    Add coverage to a few missing lines

--- collector_test.go
+++ collector_test.go
@@ -56,7 +56,7 @@ func (s *ColSuite) TestWaitCollect(c *C) {
 	c.Check(res[2], DeepEquals, &TaskResult{Value: 3, Err: nil})
 }
 
-func (s *ColSuite) TestWaitCollectMultiple(c *C) {
+func (s *ColSuite) TestWaitCollectMultipleWaits(c *C) {
 	col := NewCollector()
 
 	col.Run(func(data ...interface{}) *TaskResult {
@@ -114,6 +114,38 @@ func (s *ColSuite) TestWaitCloserTimeout(c *C) {
 	c.Check(num, Equals, 1)
 }
 
+func (s *ColSuite) TestWaitCloserTimeoutWithResults(c *C) {
+	col := NewCollector()
+
+	col.Run(func(data ...interface{}) *TaskResult {
+		time.Sleep(15 * time.Millisecond)
+		return NewResult(1, nil)
+	})
+	col.Run(func(data ...interface{}) *TaskResult {
+		return NewResult(2, nil)
+	})
+	col.Run(func(data ...interface{}) *TaskResult {
+		time.Sleep(5 * time.Millisecond)
+		return NewResult(3, nil)
+	})
+
+	closeChan := make(chan int)
+	res, err := col.WaitCloser(1*time.Millisecond, func(res *TaskResult) {
+		resNum := res.Value.(int)
+		closeChan <- resNum
+	})
+	c.Check(res, IsNil)
+	c.Check(err, NotNil)
+	c.Check(err, Equals, ErrTimeout)
+
+	num := <-closeChan
+	c.Check(num, Equals, 2)
+	num = <-closeChan
+	c.Check(num, Equals, 3)
+	num = <-closeChan
+	c.Check(num, Equals, 1)
+}
+
 func (s *ColSuite) TestWaitCloserCollect(c *C) {
 	col := NewCollector()
 
@@ -130,6 +162,29 @@ func (s *ColSuite) TestWaitCloserCollect(c *C) {
 	})
 
 	res, err := col.WaitCloser(10*time.Millisecond, func(res *TaskResult) {})
+	c.Check(err, IsNil)
+	c.Check(res, NotNil)
+	c.Check(res[0], DeepEquals, &TaskResult{Value: 1, Err: nil})
+	c.Check(res[1], DeepEquals, &TaskResult{Value: 2, Err: nil})
+	c.Check(res[2], DeepEquals, &TaskResult{Value: 3, Err: nil})
+}
+
+func (s *ColSuite) TestWaitCloserCollectNoTimeout(c *C) {
+	col := NewCollector()
+
+	col.Run(func(data ...interface{}) *TaskResult {
+		time.Sleep(5 * time.Millisecond)
+		return NewResult(1, nil)
+	})
+	col.Run(func(data ...interface{}) *TaskResult {
+		time.Sleep(1 * time.Millisecond)
+		return NewResult(2, nil)
+	})
+	col.Run(func(data ...interface{}) *TaskResult {
+		return NewResult(3, nil)
+	})
+
+	res, err := col.WaitCloser(0, func(res *TaskResult) {})
 	c.Check(err, IsNil)
 	c.Check(res, NotNil)
 	c.Check(res[0], DeepEquals, &TaskResult{Value: 1, Err: nil})
